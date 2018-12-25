0116c2162d6f40469e925b9aad781cc05c88b373
Christian Rank <git@christianrank.de>
2018-12-25 18:48:06 +0100

    feat: getting name, setup objects

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ const outputFileName = 'out/test.json'
 const inputData = []
 let outputData
 
-glob.sync('in/source_projects/**/*.json')
+const getName = (path) => path.match(/(in|out)\/source_projects\/(.*)\/project.json/)[2]
+
+glob.sync('in/source_projects/*/project.json')
   .map((route) => {
     console.log(`read ${route}`)
-    inputData.push(JSON.parse(fs.readFileSync(route)))
+    const name = getName(route)
+    inputData[name] = {
+      mapping: {},
+      content: JSON.parse(fs.readFileSync(route)),
+    }
   })
 
-glob.sync('in/destination_project/**/*.json')
+glob.sync('in/destination_project/*/project.json')
   .map((route) => {
     console.log(`read ${route}`)
     outputData = JSON.parse(fs.readFileSync(route))
   })
+
+Object.entries(inputData).map(([key, value]) => {
+  console.log({ key })
+})
 
 const output = {
   ...outputData,
