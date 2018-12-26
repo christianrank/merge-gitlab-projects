984f0637e77730341984d6aa7edd5bfae36a0d0c
Christian Rank <git@christianrank.de>
2018-12-26 01:08:35 +0100

    WIP: refa: some refactoring, not sure if better or not

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const outputFileName = 'out/project.json'
 const inputSourceProjects = []
 let inputDestinationProject
 
+// store merged data
 const mergedData = {
   issues: [],
   merge_requests: [],
@@ -13,47 +14,47 @@ const mergedData = {
   pipelines: [],
 }
 
+// helper functions
 const getName = (path) => path.match(/(in|out)\/source_projects\/(.*)\/project.json/)[2]
+const orderByCreationDate = (a, b) => new Date(a.element.created_at) - new Date(b.element.created_at)
 
-const orderByCreationDate = (a, b) => new Date(a.created_at) - new Date(b.created_at)
-
+// get source projects
 glob.sync('in/source_projects/*/project.json')
   .map((route) => {
     console.log(`read ${route}`)
     const name = getName(route)
     const content = JSON.parse(fs.readFileSync(route))
 
-    const mapping = {}
-
-    Object.keys(mergedData).forEach((entity) => {
-      mapping[entity] = {}
-
-      content[entity].map((element) => {
-        mapping[entity][element.id] = element.id
-      })
-    })
-
-    inputSourceProjects[name] = {
-      mapping,
-      content,
-    }
+    inputSourceProjects[name] = content
   })
 
+// get the destination project
 glob.sync('in/destination_project/*/project.json')
   .map((route) => {
     console.log(`read ${route}`)
     inputDestinationProject = JSON.parse(fs.readFileSync(route))
   })
 
-Object.entries(inputSourceProjects).map(([key, value]) => {
+// go through all projects and contatinate entries of each entity into mergedData
+Object.entries(inputSourceProjects).map(([projectName, projectData]) => {
   Object.keys(mergedData).forEach((entity) => {
-    mergedData[entity] = mergedData[entity].concat(value.content[entity])
+    mergedData[entity] = mergedData[entity].concat(projectData[entity])
   })
 })
 
-mergedData.issues.sort(orderByCreationDate)
-
 Object.keys(mergedData).forEach((entity) => {
+  // keep association of id to old iid
+  mergedData[entity] = mergedData[entity].map((element) => {
+    return {
+      id: element.id,
+      oldIid: element.iid,
+      element,
+    }
+  })
+
+  // sort every entity by creation date and assign new iid's
+  mergedData[entity].sort(orderByCreationDate)
+
   let iid = 0
 
   mergedData[entity] = mergedData[entity].map((element) => {
@@ -61,15 +62,27 @@ Object.keys(mergedData).forEach((entity) => {
 
     return {
       ...element,
-      iid,
+      element: {
+        ...element.element,
+        iid,
+      },
+      newIid: iid,
     }
   })
 })
 
+const outputEntityData = {}
+
+Object.entries(mergedData).forEach(([entity, elements]) => {
+  outputEntityData[entity] = elements.map((element) => element.element) // todo: think about naming...
+})
+
+// create the output
 const output = {
   ...inputDestinationProject,
-  ...mergedData,
+  ...outputEntityData,
 }
 
+// write it to the file
 console.log(`write ${outputFileName}`)
 fs.writeFileSync(outputFileName, JSON.stringify(output, null, 2))
